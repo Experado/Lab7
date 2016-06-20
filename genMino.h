abb75f61a89241ee836f28269e6f7e0fdabc0c7c
Experado <a86464217@gmail.com>
2016-06-20 15:50:00 +0800

    To much code to copy from slide, haha!!

---
+++ genMino.h
@@ -0,0 +1,11 @@
+#ifndef MINOS_H
+#define MINOS_H
+#include "Mino.h"
+#include "MinoS.h"
+#include "MinoI.h"
+#include "MinoL.h"
+#include "MinoT.h"
+#include "MinoO.h"
+
+Mino * genMino();
+ #endif
